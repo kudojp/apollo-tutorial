27e24487d1b1197c2f2634f466a06043fe536454
kudojp <heyjudejudejude1968@gmail.com>
2021-08-01 21:46:15 +0900

    Add React component wrapped with ApolloProvider

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -1,24 +1,26 @@
 import {
   ApolloClient,
-  gql,
   NormalizedCacheObject,
+  ApolloProvider
 } from '@apollo/client';
 import { cache } from './cache';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pages from './pages';
+import injectStyles from './styles';
 
 const client = new ApolloClient<NormalizedCacheObject>({
   cache,
   uri: 'http://localhost:4000/graphql',
 });
 
-client.query({
-  query: gql`
-    query GetLaunch {
-      launch(id: 56) {
-        id
-        mission {
-          name
-        }
-      }
-    }
-  `
-}).then(result => console.log(result));
+injectStyles();
+
+// Pass the ApolloClient instance to ApolloProvider component
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Pages />
+  </ApolloProvider>,
+  document.getElementById('root')
+)
+
