830585223aedf1f5bc91318ae06921f665f6374f
kudojp <heyjudejudejude1968@gmail.com>
2021-08-01 18:55:44 +0900

    Add context as arg when instantiating server

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -8,8 +8,20 @@ const resolvers = require('./resolvers');
 const LaunchAPI = require('./datasources/launch');
 const UserAPI = require('./datasources/user');
 const store = createStore();
+const isEmail = require('isemail');
 
 const server = new ApolloServer({
+  context: async ( { req } ) => {
+    //simple auth check on every request
+    const auth = req.headers && req.headers.authorization || '';
+    const email = Buffer.from(auth, 'base64').toString('ascii');
+    if (!isEmail.validate(email)) return { user: null};
+    // find a user by their email
+    const users = await store.users.findOrCreate({where: {email}});
+    const user = users && user[0] || null;
+
+    return { user: { ...user.dataValues }}; // resolver実行時の第3引数のcontextで呼び出せるという認識
+  },
   typeDefs,
   resolvers,
   dataSources: () => ({
