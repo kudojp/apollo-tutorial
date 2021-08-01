94b482a8e13f602ea106d3b781423aa4fb5a7eba
kudojp <heyjudejudejude1968@gmail.com>
2021-08-01 18:23:29 +0900

    Add pagination to getAllLaunches

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -1,7 +1,24 @@
+const { paginateResults } = require('./utils');
+
 module.exports = {
   Query: {
-    launches: (_, __, { dataSources }) =>
-      dataSources.launchAPI.getAllLaunches(),
+    launches: async (_, { pageSize = 20, after }, { dataSources }) => {
+      const allLaunches = await dataSources.launchAPI.getAllLaunches();
+      // we want these in reverse chronological order
+      allLaunches.reverse();
+      const launches = paginateResults({
+        after,
+        pageSize,
+        results: allLaunches
+      });
+      return {
+        launches,
+        cursor: launches.length ? launches[launches.length - 1].cursor : null, // launchesの最後の要素
+        hasMore: launches.length
+          ? launches[launches.length - 1].cursor !== allLaunches[allLaunches.length - 1].cursor
+          : false
+      };
+    },
 
     launch: (_, { id }, { dataSources }) =>
       dataSources.launchAPI.getLaunchById({ launchId: id }),
--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -2,7 +2,16 @@ const { gql } = require('apollo-server');
 
 const typeDefs = gql`
   type Query {
-    launches: [Launch]!
+    launches(
+      """
+      The number of results to show. Must be >= 1. Default = 20
+      """
+      pageSize: Int
+      """
+      If you add a cursor here, it will only return results_after_ this cursor.
+      """
+      after: String
+    ): LaunchConnection!
     launch(id: ID!): Launch
     # Queries for the current user.
     me: User
@@ -14,6 +23,16 @@ const typeDefs = gql`
     mission: Mission
     rocket: Rocket
     isBooked: Boolean!
+  }
+  """
+  Simple wrapper around our list of launches that contains a cursor to the
+  last item in the list. Pass this cursor to the launches query to fetch results
+  after these.
+  """
+  type LaunchConnection {
+    cursor: String!
+    hasMore: Boolean!
+    launches: [Launch]!
   }
 
   type Rocket {
